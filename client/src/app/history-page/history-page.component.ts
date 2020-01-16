0c4a878bf0a4a33f6263851c7af932017731e66f
Anton A. Dymkov <aadym@mail.ru>
2020-01-16 11:51:23 +0300

    initial

---
+++ client/src/app/history-page/history-page.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy, AfterViewInit } from '@angular/core'
+import { Subscription } from 'rxjs'
+import { MaterialInstance, MaterialService } from '../shared/classes/material.service'
+import { OrdersService } from '../shared/services/orders.service'
+import { Order, Filter } from '../shared/interfaces'
+
+const STEP = 2
+
+@Component({
+  selector: 'app-history-page',
+  templateUrl: './history-page.component.html',
+  styleUrls: ['./history-page.component.css']
+})
+export class HistoryPageComponent implements OnInit, OnDestroy, AfterViewInit {
+
+  @ViewChild('tooltip', { static: false }) tooltipRef: ElementRef
+  tooltip: MaterialInstance
+  oSub: Subscription
+  ifFilterVisible = false
+  orders: Order[] = []
+  filter: Filter = {}
+
+  offset = 0
+  limit = STEP
+
+  loading = false
+  reloading = false
+  noMoreOrders = false
+
+  constructor(private ordersService: OrdersService) { }
+
+  ngOnInit() {
+    this.reloading = true
+    this.fetch()
+  }
+
+  private fetch() {
+
+    const params = Object.assign({}, this.filter, {
+      offset: this.offset,
+      limit: this.limit
+    })
+
+    this.oSub = this.ordersService.fetch(params).subscribe(
+      orders => {
+        this.orders = this.orders.concat(orders)
+        this.noMoreOrders = orders.length < STEP
+        this.loading = false
+        this.reloading = false
+      }
+    )
+  }
+
+  loadMore() {
+    this.offset += STEP
+    this.loading = true
+    this.fetch()
+  }
+
+  ngOnDestroy() {
+    this.tooltip.destroy()
+    if (this.oSub) {
+      this.oSub.unsubscribe()
+    }
+  }
+
+  applyFilter(filter: Filter) {
+    this.orders = []
+    this.offset = 0
+    this.reloading = true
+    this.filter = filter
+    this.fetch()
+  }
+
+  ngAfterViewInit() {
+    this.tooltip = MaterialService.initTooltip(this.tooltipRef)
+  }
+
+  isFiltered(): boolean {
+    return Object.keys(this.filter).length !== 0
+  }
+
+}
